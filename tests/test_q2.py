aa7e45b87c5e3b68b79bef1e4ed53ec2fea33d06
wenzhangliu <liu_wzh@foxmail.com>
2025-10-24 16:37:46 +0800

    update

--- tests/test_q2.py
+++ tests/test_q2.py
@@ -1,33 +1,33 @@
 from src.q2_sum_filter import special_sum
 
 
-def q2_sum_filter_task_1():
+def test_q2_sum_filter_task_1():
     assert special_sum(1) == 0
 
 
-def q2_sum_filter_task_2():
+def test_q2_sum_filter_task_2():
     assert special_sum(2) == 0
 
 
-def q2_sum_filter_task_3():
+def test_q2_sum_filter_task_3():
     assert special_sum(3) == 3
 
 
-def q2_sum_filter_task_4():
+def test_q2_sum_filter_task_4():
     assert special_sum(4) == 3
 
 
-def q2_sum_filter_task_5():
+def test_q2_sum_filter_task_5():
     assert special_sum(5) == 8
 
 
-def q2_sum_filter_task_6():
+def test_q2_sum_filter_task_6():
     assert special_sum(10) == 33
 
 
-def q2_sum_filter_task_7():
+def test_q2_sum_filter_task_7():
     assert special_sum(15) == 45
 
 
-def q2_sum_filter_task_8():
-    assert special_sum(100) == 2003
+def test_q2_sum_filter_task_8():
+    assert special_sum(100) == 2103
